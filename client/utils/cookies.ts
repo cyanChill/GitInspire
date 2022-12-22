9176a1a98992ae7161a43d9c3a6127789832cbe4
cyanChill <anthonyliang9@gmail.com>
2022-12-21 20:25:22 -0500

    solve typescript issue with pop()

--- client/utils/cookies.ts
+++ client/utils/cookies.ts
@@ -2,6 +2,7 @@ export const getCookie = (name: String) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) {
-    return parts.pop().split(";").shift();
+    const last_entry = parts.pop();
+    if (last_entry) return last_entry.split(";").shift();
   }
 };
