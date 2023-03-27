04e36479771e57ad851fea78435dd5d385d1bf7e
cyanChill <anthonyliang9@gmail.com>
2023-03-26 23:16:02 -0400

    basic user profile page w/ refresh route

--- backend/server/routes/user.py
+++ backend/server/routes/user.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, g, request, jsonify, url_for
+from flask import Blueprint, g, request, jsonify, current_app as app
 from flask_jwt_extended import jwt_required
+from sqlalchemy import update
+import requests
 import traceback
 
+from server.utils import isXDayOld
 from server.db import db
 from server.models.User import User
 
-bp = Blueprint("user", __name__, url_prefix="/user")
+bp = Blueprint("user", __name__, url_prefix="/users")
 
 # Route to get all users
 @bp.route("/")
@@ -19,15 +22,92 @@ def get_user(userId):
     user = User.query.filter_by(id=userId).first()
 
     if user != None:
-        return jsonify({"message": "User found.", "user": user.as_dict()}), 200
+        response = {
+            "message": "User found.",
+            "user": user.as_dict(),
+            "contributions": user.contributions(),
+        }
+        return jsonify(response), 200
     else:
-        return jsonify({"message": "User not found.", "user": None}), 200
+        response = {"message": "User found.", "user": None, "contributions": None}
+        return jsonify(response), 200
 
 
 # Route to refresh user info from GitHub API
 @bp.route("/<int:userId>/refresh")
 def refresh_user(userId):
-    return jsonify({"message": "Route to 'refresh' user info."})
+    # Get local copy of user & check it hasn't been updated in the last day
+    existing_user = User.query.filter_by(id=userId).first()
+    if existing_user == None:
+        response = {"message": f"User with user id {userId} doesn't exist."}
+        return jsonify(response), 404
+
+    if not isXDayOld(existing_user.last_updated, 1):
+        response = {
+            "message": "User has been recently updated.",
+            "user": existing_user.as_dict(),
+        }
+        return jsonify(response), 200
+
+    # Call GitHub API since user data can be refreshed
+    user_data_resp = requests.get(
+        f"https://api.github.com/user/{userId}",
+        auth=(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"]),
+        headers={
+            "Accept": "application/vnd.github.text-match+json",
+            "User-Agent": "gitinspire-server",
+        },
+    )
+
+    # Handle case where rate limit was hit, validation failed, endpoint has been spammed
+    if user_data_resp.status_code in [403, 422]:
+        return jsonify({"message": "Something else went wrong."}), 500
+
+    # Handle case where user is no longer accessible via the API
+    if user_data_resp.status_code == 404:
+        response = {
+            "message": "User is no longer accessible via the GitHub API.",
+            "user": existing_user.as_dict(),
+        }
+        return jsonify(response), 410
+
+    # Handle case where no modifications was made
+    if user_data_resp.status_code == 304:
+        # "Touch" User entry to trigger onupdate event to update "last_update"
+        stmt = update(User).where(User.id == userId)
+        db.engine.execute(stmt)
+        response = {
+            "message": "No changes was found.",
+            "user": existing_user.as_dict(),
+        }
+        return jsonify(response), 200
+
+    # Some other untracked error
+    if not user_data_resp.ok:
+        return jsonify({"message": "An unknown error has occurred."}), 500
+
+    # Handling case where modifications were made
+    updt_user_data = user_data_resp.json()
+    update_dict = {
+        "username": updt_user_data["login"],
+        "avatar_url": updt_user_data["avatar_url"],
+    }
+
+    try:
+        # Commiting update
+        update_stmt = update(User).filter_by(id=userId).values(**update_dict)
+        db.session.execute(update_stmt)
+        db.session.commit()
+    except:
+        print(traceback.format_exc())
+        response = {"message": "Something went wrong with refreshing user data."}
+        return jsonify(response), 500
+
+    response = {
+        "message": "Refreshed user information.",
+        "user": existing_user.as_dict(),
+    }
+    return jsonify(response), 200
 
 
 # Route to get the tags & repositories user has contributed to GitInspire
--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -1,3 +1,190 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+import { UserObjType, TagObjType, RepositoryObjType } from "~utils/types";
+import { isXDaysOld, cleanDate } from "~utils/helpers";
+import useUserContext from "~hooks/useUserContext";
+import Spinner from "~components/Spinner";
+import PageRedirectForm from "~components/page_forms/PageRedirectForm";
+import Button from "~components/form/Button";
+
+type ContributionType = {
+  suggested_tags: TagObjType[];
+  suggested_repos: RepositoryObjType[];
+};
+
 export default function UserProfilePage() {
-  return <div className="animate-load-in">User Profile Page</div>;
+  const router = useRouter();
+  const { isAuthenticated } = useUserContext();
+
+  const [user, setUser] = useState<UserObjType>();
+  const [contributions, setContributions] = useState<ContributionType>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const handleReport = () => {
+    // Redirect to the report route with some information in the URL params
+    router.push(`/report?type=user&id=${user?.id}`);
+  };
+
+  const refreshInfo = async () => {
+    if (!user) return;
+    // User data has been refreshed recently
+    if (!isXDaysOld(user.last_updated, 1)) return;
+
+    setIsRefreshing(true);
+    const res = await fetch(`/api/users/${user.id}/refresh`);
+    if (!res.ok) {
+      /* Error occurred */
+      // Note: Status Code 410 indicates user can't be found with GitHub API
+      if (res.status == 410) toast.error("User is no longer found on GitHub .");
+      else toast.error("Something went wrong on our servers.");
+    } else {
+      /* Found user */
+      const data = await res.json();
+      console.log(data);
+      setUser(data.user);
+    }
+    setIsRefreshing(false);
+  };
+
+  useEffect(() => {
+    let abortCtrl = new AbortController();
+
+    if (router.query && router.query.id) {
+      setIsLoading(true);
+
+      fetch(`/api/users/${router.query.id}`, {
+        signal: abortCtrl.signal,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (!data) {
+            setUser(undefined);
+            setContributions(undefined);
+            toast.error("User not found.");
+          } else if (data.user) {
+            setUser(data.user);
+            setContributions(data.contributions);
+          }
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          // Handle non-AbortError(s)
+          if (err.name !== "AbortError") {
+            setUser(undefined);
+            setContributions(undefined);
+            toast.error("Something went wrong with fetching user.");
+            setIsLoading(false);
+          }
+        });
+    }
+
+    return () => {
+      abortCtrl.abort();
+    };
+  }, [router.query]);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  } else if (!user) {
+    return (
+      <div className="flex h-full animate-load-in flex-col items-center justify-center">
+        <h1 className="text-center text-4xl font-bold">
+          This user does not exist
+        </h1>
+        <p className="my-6 text-center">
+          Sorry, but we couldn&apos;t find this user in our database.
+        </p>
+        <PageRedirectForm />
+      </div>
+    );
+  }
+
+  return (
+    <div className="animate-load-in bg-white p-2 shadow-md dark:bg-slate-800 dark:shadow-slate-700">
+      <div className="flex min-w-0 items-end gap-2 border-b-2 pb-2">
+        <Image
+          src={user?.avatar_url ?? "/assets/default_avatar.png"}
+          alt={`${user?.username} profile picture`}
+          width={64}
+          height={64}
+          className="max-w-fit rounded-md bg-slate-800 dark:bg-white"
+        />
+        <a
+          href={`https://github.com/${user.username}`}
+          target="_blank"
+          rel="noreferrer"
+          className="truncate text-2xl font-semibold hover:underline"
+        >
+          {user.username}
+        </a>
+      </div>
+
+      <p className="my-2 font-semibold underline">
+        ({contributions?.suggested_tags.length ?? 0}) Contributed Tags:
+      </p>
+      <div className="flex min-w-0 flex-wrap gap-1">
+        {contributions?.suggested_tags.map((tg) => (
+          <span
+            key={tg.name}
+            className="truncate rounded-xl bg-neutral-200 px-2 py-0.5 dark:bg-slate-700"
+          >
+            {tg.display_name}
+          </span>
+        ))}
+      </div>
+
+      <p className="my-2 font-semibold underline">
+        ({contributions?.suggested_repos.length ?? 0}) Contributed Repositories:
+      </p>
+      <div className="flex min-w-0 flex-col">
+        {contributions?.suggested_repos.map((repo) => (
+          <Link
+            key={repo.id}
+            href={`/repository/${repo.id}`}
+            className={`my-1 truncate rounded-md border-[1px] border-slate-300 p-1.5 hover:cursor-pointer hover:bg-slate-300 hover:underline dark:border-slate-600 dark:hover:bg-slate-600`}
+          >
+            {repo.author}/{repo.repo_name}
+          </Link>
+        ))}
+      </div>
+
+      {/* Action */}
+      <hr className="my-2" />
+      <p className="my-1 px-2 text-right text-sm italic text-slate-500">
+        Last Updated: {cleanDate(user.last_updated)}
+      </p>
+      <div className="flex flex-wrap justify-end gap-3 px-2 text-sm">
+        {isAuthenticated && (
+          <button
+            className="text-slate-500 hover:underline dark:text-gray-50"
+            onClick={handleReport}
+          >
+            Report a Problem
+          </button>
+        )}
+        {/* Allow refresh button for non-authenticated users */}
+        <Button
+          className="my-0 w-max"
+          clr={{
+            bkg: "bg-indigo-400 enabled:hover:bg-indigo-500 disabled:opacity-25",
+            txt: "text-white",
+          }}
+          onClick={refreshInfo}
+          disabled={!isXDaysOld(user.last_updated, 1) || isRefreshing}
+        >
+          Refresh Data
+        </Button>
+      </div>
+    </div>
+  );
 }
