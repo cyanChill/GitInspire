3c07331a78bc30481771c7b055d799cb30ef2095
cyanChill <anthonyliang9@gmail.com>
2023-05-03 00:01:40 -0400

    updated profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -3,13 +3,15 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 import toast from "react-hot-toast";
+import { FaCrown, FaGithub } from "react-icons/fa";
+import { TbShieldCheckFilled } from "react-icons/tb";
 
 import { UserObjType, TagObjType, RepositoryObjType } from "~utils/types";
 import { isXDaysOld, cleanDate } from "~utils/helpers";
 import useUserContext from "~hooks/useUserContext";
-import Spinner from "~components/Spinner";
 import PageRedirectForm from "~components/page_forms/PageRedirectForm";
 import Button from "~components/form/Button";
+import { LazyText } from "~components/Loading";
 import SEO from "~components/layout/SEO";
 
 type ContributionType = {
@@ -86,16 +88,7 @@ export default function UserProfilePage() {
     };
   }, [router.query]);
 
-  if (isLoading) {
-    return (
-      <>
-        <SEO pageName="Profile" />
-        <div className="flex h-full items-center justify-center">
-          <Spinner />
-        </div>
-      </>
-    );
-  } else if (!user) {
+  if (!isLoading && !user) {
     return (
       <>
         <SEO pageName="Profile" />
@@ -112,86 +105,158 @@ export default function UserProfilePage() {
     );
   }
 
+  const numCtbTags = contributions?.suggested_tags.length ?? 0;
+  const numCtbRepos = contributions?.suggested_repos.length ?? 0;
+
   return (
     <>
-      <SEO pageName={`${user.username} Profile`} />
-      <div className="animate-load-in bg-white p-2 shadow-md dark:bg-slate-800 dark:shadow-slate-700">
-        <div className="flex min-w-0 items-end gap-2 border-b-2 pb-2">
-          <Image
-            src={user?.avatar_url ?? "/assets/default_avatar.png"}
-            alt={`${user?.username} profile picture`}
-            width={64}
-            height={64}
-            className="max-w-fit rounded-md bg-slate-800 dark:bg-white"
-          />
-          <a
-            href={`https://github.com/${user.username}`}
-            target="_blank"
-            rel="noreferrer"
-            className="truncate text-2xl font-semibold hover:underline"
-          >
-            {user.username}
-          </a>
-        </div>
+      <SEO pageName={isLoading ? "Profile" : `${user?.username} Profile`} />
 
-        <p className="my-2 font-semibold underline">
-          ({contributions?.suggested_tags.length ?? 0}) Contributed Tags:
-        </p>
-        <div className="flex min-w-0 flex-wrap gap-1">
-          {contributions?.suggested_tags.map((tg) => (
-            <span
-              key={tg.name}
-              className="truncate rounded-xl bg-neutral-200 px-2 py-0.5 dark:bg-slate-700"
+      <main className="animate-load-in font-sourceCodePro">
+        {/* Profile Picture + Name */}
+        <header className="flex gap-4">
+          {isLoading ? (
+            <div className="lazy-bg h-16 w-16 shrink-0 animate-lazy-bg border border-[1px] border-slate-600 bg-stone-950 p-2 shadow-[5px_5px] shadow-orange-p-600 dark:border-slate-300 dark:bg-white min-[400px]:h-24 min-[400px]:w-24" />
+          ) : (
+            <Image
+              src={user?.avatar_url ?? "/assets/default_avatar.png"}
+              alt={`${user?.username} profile picture`}
+              width={96}
+              height={96}
+              className="h-16 w-16 shrink-0 border border-[1px] border-slate-600 bg-stone-950 p-2 shadow-[5px_5px] shadow-orange-p-600 dark:border-slate-300 dark:bg-white min-[400px]:h-24 min-[400px]:w-24"
+            />
+          )}
+          {isLoading ? (
+            <LazyText dimensionStyle="self-end h-4 w-full min-[400px]:w-32 min-[400px]:h-6" />
+          ) : (
+            <a
+              href={`https://github.com/${user?.username}`}
+              target="_blank"
+              rel="noreferrer"
+              className="flex min-w-0 gap-2 items-center self-end text-xl font-semibold hover:underline min-[400px]:text-3xl"
             >
-              {tg.display_name}
-            </span>
-          ))}
-        </div>
+              <span className="truncate">{user?.username}</span>
+              <FaGithub className="shrink-0" />
+              {user?.account_status === "admin" && (
+                <TbShieldCheckFilled className="shrink-0 text-purple-500" />
+              )}
+              {user?.account_status === "owner" && (
+                <FaCrown className="shrink-0 text-orange-p-600" />
+              )}
+            </a>
+          )}
+        </header>
 
-        <p className="my-2 font-semibold underline">
-          ({contributions?.suggested_repos.length ?? 0}) Contributed
-          Repositories:
-        </p>
-        <div className="flex min-w-0 flex-col">
-          {contributions?.suggested_repos.map((repo) => (
-            <Link
-              key={repo.id}
-              href={`/repository/${repo.id}`}
-              className={`my-1 truncate rounded-md border-[1px] border-slate-300 p-1.5 hover:cursor-pointer hover:bg-slate-300 hover:underline dark:border-slate-600 dark:hover:bg-slate-600`}
-            >
-              {repo.author}/{repo.repo_name}
-            </Link>
-          ))}
-        </div>
+        {/* Contribution Information */}
+        {isLoading ? (
+          <>
+            <LazyText dimensionStyle="w-full min-[400px]:w-96 h-4 mt-4" />
+            <LazyText dimensionStyle="w-full h-9 mt-4" />
+            <LazyText dimensionStyle="w-full h-9 mt-4" />
+          </>
+        ) : (
+          <>
+            <p className="mt-4 text-sm">
+              They contributed{" "}
+              <span className="font-semibold">{numCtbTags}</span> tags and{" "}
+              <span className="font-semibold">{numCtbRepos}</span> repositories
+              to GitInspire.
+            </p>
+
+            <ProfileAccordion amount={numCtbTags} variant="Tags">
+              <div className="flex min-w-0 flex-wrap gap-1">
+                {contributions?.suggested_tags.map((tg) => (
+                  <span
+                    key={tg.name}
+                    className="truncate rounded-xl bg-neutral-200 px-2 py-0.5 dark:bg-slate-700"
+                  >
+                    {tg.display_name}
+                  </span>
+                ))}
+              </div>
+            </ProfileAccordion>
+
+            <ProfileAccordion amount={numCtbRepos} variant="Repositories">
+              <div className="flex min-w-0 flex-col gap-1">
+                {contributions?.suggested_repos.map((repo) => (
+                  <Link
+                    key={repo.id}
+                    href={`/repository/${repo.id}`}
+                    className={`my-1 truncate rounded-md border-[1px] border-slate-300 p-1.5 hover:cursor-pointer hover:bg-slate-200 hover:underline dark:border-slate-600 dark:hover:bg-slate-600`}
+                  >
+                    {repo.author}/{repo.repo_name}
+                  </Link>
+                ))}
+              </div>
+            </ProfileAccordion>
+          </>
+        )}
 
         {/* Action */}
-        <hr className="my-2" />
-        <p className="my-1 px-2 text-right text-sm italic text-slate-500">
-          Last Updated: {cleanDate(user.last_updated)}
-        </p>
-        <div className="flex flex-wrap justify-end gap-3 px-2 text-sm">
-          {isAuthenticated && (
-            <button
-              className="text-slate-500 hover:underline dark:text-gray-50"
-              onClick={handleReport}
-            >
-              Report a Problem
-            </button>
-          )}
-          {/* Allow refresh button for non-authenticated users */}
-          <Button
-            className="my-0 w-max"
-            clr={{
-              bkg: "bg-indigo-400 enabled:hover:bg-indigo-500 disabled:opacity-25",
-              txt: "text-white",
-            }}
-            onClick={refreshInfo}
-            disabled={!isXDaysOld(user.last_updated, 1) || isRefreshing}
-          >
-            Refresh Data
-          </Button>
+        {!isLoading && user && (
+          <>
+            <hr className="my-2" />
+            <p className="my-1 px-2 text-right text-sm italic text-slate-500">
+              Last Updated: {cleanDate(user.last_updated)}
+            </p>
+            <div className="flex flex-wrap justify-end gap-3 px-2 text-sm">
+              {isAuthenticated && (
+                <button
+                  className="text-slate-500 hover:underline dark:text-gray-50"
+                  onClick={handleReport}
+                >
+                  Report a Problem
+                </button>
+              )}
+              {/* Allow refresh button for non-authenticated users */}
+              <Button
+                className="my-0 w-max"
+                clr={{
+                  bkg: "bg-teal-p-600 enabled:hover:bg-teal-p-700 disabled:opacity-25",
+                  txt: "text-white",
+                }}
+                onClick={refreshInfo}
+                disabled={!isXDaysOld(user.last_updated, 1) || isRefreshing}
+              >
+                Refresh Data
+              </Button>
+            </div>
+          </>
+        )}
+      </main>
+    </>
+  );
+}
+
+type ProfileAccordionType = {
+  amount: number;
+  variant: "Tags" | "Repositories";
+  children: React.ReactNode;
+};
+
+function ProfileAccordion({ amount, variant, children }: ProfileAccordionType) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="my-4 rounded-md bg-white dark:bg-slate-800">
+      <p
+        className="p-2 text-sm hover:cursor-pointer"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <span className="rounded-md bg-neutral-100 p-1 font-semibold dark:bg-slate-900">
+          {amount}
+        </span>{" "}
+        {variant}
+      </p>
+      <div
+        className={`grid text-xs transition-[grid-template-rows] duration-300 ${
+          isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
+        }`}
+      >
+        <div className="overflow-hidden px-2">
+          <div className="my-2">{children}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
